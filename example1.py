680327596a81f1e8df5b2ba949c5a39b6f8b7992
smarrero <sergio.marrero@cornerjob.com>
2020-10-05 18:39:00 +0200

    adding functions to document1

---
+++ example1.py
@@ -0,0 +1,22 @@
+def function1(arg1, arg2):
+    """This functions take two arguments and returns the secoond
+
+    :param arg1: some value
+    :param arg2: another value
+    :return: arg1
+    """
+    arg1 = arg2
+    return arg1
+
+
+def function2(arg1, arg2):
+    """
+
+    :param arg1:
+    :param arg2:
+    :return:
+    """
+
+    arg1 = arg2
+    return arg1
+
